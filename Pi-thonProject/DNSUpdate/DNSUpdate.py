914715eba8c6ea5a9a59fbcda811e896816a2919
Hobnob <williamhobson62@aol.com>
2015-10-25 19:25:28 +0000

    a

--- Pi-thonProject/DNSUpdate/DNSUpdate.py
+++ Pi-thonProject/DNSUpdate/DNSUpdate.py
@@ -1,2 +1,3 @@
 import httplib
-httplib.HTTPConnection("http://freedns.afraid.org/dynamic/update.php?YTVEWUpTcmdvTFFKVzdwRE1zbG5rb1RlOjE1MjEyMDYw")
+HTTPConnection = httplib.HTTPConnection("http://freedns.afraid.org/dynamic/update.php?YTVEWUpTcmdvTFFKVzdwRE1zbG5rb1RlOjE1MjEyMDYw")
+HTTPConnection.request("")
