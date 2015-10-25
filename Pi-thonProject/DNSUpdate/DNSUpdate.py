e82f5c8a36ea856a3a0d46bb2c378628cf6924ce
Hobnob <williamhobson62@aol.com>
2015-10-25 19:45:17 +0000

    maby this will work who knows

--- Pi-thonProject/DNSUpdate/DNSUpdate.py
+++ Pi-thonProject/DNSUpdate/DNSUpdate.py
@@ -1,2 +1,3 @@
 import httplib
-print(httplib.HTTPConnection("freedns.afraid.org",80).request("GET","/dynamic/update.php?YTVEWUpTcmdvTFFKVzdwRE1zbG5rb1RlOjE1MjEyMDYw").getresponce().read())
+conn = httplib.HTTPConnection("freedns.afraid.org",80).request("GET","/dynamic/update.php?YTVEWUpTcmdvTFFKVzdwRE1zbG5rb1RlOjE1MjEyMDYw")
+print(conn.getresponce().read())
