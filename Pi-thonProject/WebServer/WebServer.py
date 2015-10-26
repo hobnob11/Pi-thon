93b2b5eb38a319a910c64c43a79fff8014fd1883
Hobnob <williamhobson62@aol.com>
2015-10-26 16:31:14 +0000

    what are the chances this will work

--- Pi-thonProject/WebServer/WebServer.py
+++ Pi-thonProject/WebServer/WebServer.py
@@ -9,6 +9,7 @@ GPIO.setup(5,GPIO.OUT)
 State = 0
 Allowed = ["Public"]
 Requests = []
+
 def SwitchState(A):
     global State
     State = A
@@ -22,12 +23,20 @@ def CheckIfAllowed(A):
         return -1
     else:
         return index
+def CheckIfRequested(A):
+    global Requested
+    try:
+        index = Requested.index(A)
+    except ValueError:
+        return False
+    else:
+        return True
 
 @app.route('/')
 def index():
     #if the incomming request is on the local network
     if(str(request.remote_addr)[:10] == "192.168.1."):
-        return render_template("admin.html",Allowed = Allowed)
+        return render_template("admin.html",Allowed = Allowed,Requested = Requested)
     else:
         return render_template("index.html")
 
@@ -46,11 +55,9 @@ def Output():
     global State
     return str(State) + " :D"
 
-@app.route('/Input/<Id>/<Input>')
-def Input(Id,Input):
-    global State
-    print(str(CheckIfAllowed(Id)))
-    if(CheckIfAllowed(Id)!=-1):
+@app.route('/Input/Public/<Input>')
+def PublicInput(Input):
+    if(CheckIfAllowed("Public")!=-1):
         if(Input=="1"):
             SwitchState(1)
         if(Input=="0"):
@@ -58,7 +65,36 @@ def Input(Id,Input):
         return str(State) + "   " + str(Input)
     else:
         return "Access Denied"
-        
+
+@app.route('/Input/<Id>/<Input>')
+def Input(Id,Input):
+    global State
+    if(CheckIfAllowed(str(request.remote_addr) + str(Id))!=-1):
+        if(Input=="1"):
+            SwitchState(1)
+        if(Input=="0"):
+            SwitchState(0)
+        return str(State) + "   " + str(Input)
+    else:
+        global Requests
+        if(CheckIfRequested(str(request.remote_addr) + str(Id))==False):
+            Requests.append(str(request.remote_addr) + str(Id))
+        return "Access Denied"
+
+@app.route('/Input/<Input>')
+def Input2(Input):
+    global State
+    global Requests
+    if(CheckIfAllowed(request.remote_addr)!=-1):
+        if(Input=="1"):
+            SwitchState(1)
+        if(Input=="0"):
+            SwitchState(0)
+        return str(State) + "   " + str(Input)
+    else:
+        if(CheckIfRequested(str(request.remote_addr))==False):
+            Requests.append(str(request.remote_addr))
+        return "Access Denied"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=80)
